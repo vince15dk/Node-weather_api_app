1a3b802eefe835e973edcf1789360b5f3da4f66e
SJ <vince15dk@gmail.com>
2019-03-03 16:08:39 +0900

    added request module

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const request = require('request')
 
+const url = "https://api.darksky.net/forecast/785fa09a9c9259fe91a7c9c40930b6c4/37.8267,-122.4233"
+
+request({url},(err, response)=>{
+    const data = JSON.parse(response.body)
+})
