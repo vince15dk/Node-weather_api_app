f20eb11ad638fd7644ca16f9946a6816e6562cb2
SJ <vince15dk@gmail.com>
2019-03-03 20:00:40 +0900

    address chaining

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ const chalk = require('chalk')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const address = process.argv[2]
 
-
-
-
-geocode('busan', (err, data)=>{
-    if(err){
-        console.log('Err', err)
-        return
-    }
-    console.log('Data', data)
-    forecast(data.latitude, data.longitude,(err, data)=>{
+if(!address){
+    console.log('Please provide an address')
+} else {
+    geocode(address, (err, data)=>{
         if(err){
             console.log('Err', err)
             return
         }
-        console.log('Data', data)
+        forecast(data.latitude, data.longitude,(err, forecastData)=>{
+            if(err){
+                console.log('Err', err)
+                return
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    
     })
-
-})
+}
 
 // geocode('seoul', (err, data)=>{
 //     if(err){
