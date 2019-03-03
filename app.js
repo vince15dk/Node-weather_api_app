de40e0d64f37cb298dddc1adea66dc286e47a598
SJ <vince15dk@gmail.com>
2019-03-03 17:26:17 +0900

    added error handling

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const request = require('request')
 const chalk = require('chalk')
-const {url} = require('./config/config')
+const {url, geoCode} = require('./config/config')
 
 request({url, json: true},(err, response)=>{
     if(err){
-        console.log(chalk.bold.red.inverse('error exists'))
-    } else {
+        console.log(chalk.bold.red.inverse('Unable to connect to weather service!'))
+    } else if (response.body.error){
+        console.log('Unable to find location')
+    } 
+    else {
         console.log(`It is currently ${chalk.bold.blue.inverse(response.body.currently.temperature)}`)
         console.log(`${chalk.bold.blue.inverse(response.body.daily.summary)}`)
         console.log(`${chalk.bold.red.inverse(response.body.daily.data[0].summary)}`)
     }
    
 })
+
+request({url: geoCode, json:true},(err, response)=>{
+    if(err){
+        console.log(chalk.bold.red.inverse('Unable to connect to geocode service!')) 
+    } else if(response.body.features.length === 0){
+        console.log('Unable to find location')
+    }
+    else {
+        const latitude = response.body.features[0].geometry.coordinates[0];
+        const longitude = response.body.features[0].geometry.coordinates[1];
+
+        console.log(latitude)
+        console.log(longitude)
+    }
+})
+
